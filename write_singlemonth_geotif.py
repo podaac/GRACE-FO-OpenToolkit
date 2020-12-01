f5875cdccafea550a2e7a6f80798ea028c27fec1
whl520 <30417343+whl520@users.noreply.github.com>
2020-12-01 13:51:32 -0800

    Update write_singlemonth_geotif.py

--- write_singlemonth_geotif.py
+++ write_singlemonth_geotif.py
@@ -1,20 +1,19 @@
+#!/usr/bin/env python3
+
 # This python script reads the GRACE mascon Level 3 data available in netcdf
 # 'GRCTellus.JPL.*.RL06M.MSCNv02CRI.nc' and creates geotiff for each time step.
 #
 # Each grace timestep has two geotiffs, one for variable lwe_thickness and other for uncertainty.
-# Users need to download the GRACE mascon netcdf file and set its location in the variable input_mascon_file (line #26) ;
-# Additionally users need to specify in line # 27 the location of output geotiff.
+# Users need to download the GRACE mascon netcdf file and set its location in the variable input_mascon_file (line #25) ;
+# Additionally users need to specify in line # 26 the location of output geotiff.
 # For any comments and feedback, please contact: podaac@jpl.nasa.gov
 
 import datetime
-from datetime import date
 from pathlib import Path
 
-from netCDF4 import Dataset
-
 import numpy as np
-import pandas as pd
 
+from netCDF4 import Dataset, num2date
 from osgeo import gdal, osr
 
 # Data can be downloaded from here: 
@@ -23,151 +22,153 @@ from osgeo import gdal, osr
 # You will need an EarthData account to access the PO.DAAC drive.
 
 # replace the NetCDF (nc) filename with current mascon netcdf name as it gets updated every month
+# input_mascon_file = Path('PATH/TO/YOUR/INPUTFILE')
+# output_geotiff_path = Path('/PATH/TO/YOUR/OUTPUTFILE')
+
 input_mascon_file = Path() / 'input' / 'GRCTellus.JPL.200204_202008.GLO.RL06M.MSCNv02CRI.nc' 
 output_geotiff_path = Path() / 'output'
 output_geotiff_path.mkdir(parents=True, exist_ok=True)
 
+def gtiff_write(grid_data, data_res, output_geotiff_file, lat_bound_max, lon_bound_min):
+    gdal_driver = gdal.GetDriverByName('GTiff')
 
-def GTiff_Write(grid_data, sr, output_geotiff_file):
-    Gdal_Driver = gdal.GetDriverByName('GTiff')
-    GTiff_Name = str(output_geotiff_file)
-    GTiff_Out = Gdal_Driver.Create(GTiff_Name, grid_data.shape[1], grid_data.shape[0], 1, gdal.GDT_Float32)
-    Out_GeoTransform = [-180, sr, 0, 90, 0, -1*sr]
-    GTiff_Out.SetGeoTransform(Out_GeoTransform)
-    Out_Wkt = osr.SpatialReference()
-    Out_Wkt.ImportFromEPSG(4326)
-    Out_Proj_Wkt = Out_Wkt.ExportToWkt()
-    GTiff_Out.SetProjection(Out_Proj_Wkt)
-    GTiff_Band_Out = GTiff_Out.GetRasterBand(1)
-    GTiff_Band_Out.SetNoDataValue(-99999)
-    GTiff_Band_Out.WriteArray(grid_data)
-    GTiff_Out = None
-    GTiff_Band_Out = None
+    gtiff_name = str(output_geotiff_file)
+    gtiff_out = gdal_driver.Create(gtiff_name, grid_data.shape[1], grid_data.shape[0], 1, gdal.GDT_Float32)
+
+    out_geotransform = [lon_bound_min, data_res, 0, lat_bound_max, 0, -1*data_res]
+    gtiff_out.SetGeoTransform(out_geotransform)
+    
+    out_wkt = osr.SpatialReference()
+    out_wkt.ImportFromEPSG(4326)
+    out_proj_wkt = out_wkt.ExportToWkt()
+    gtiff_out.SetProjection(out_proj_wkt)
+    
+    gtiff_band_out = gtiff_out.GetRasterBand(1)
+    gtiff_band_out.SetNoDataValue(-99999)
+    gtiff_band_out.WriteArray(grid_data)
+
+    gtiff_out = None
+    gtiff_band_out = None
 
 
-def GTiff_SetMetadata(output_geotiff_file, fh, current_month_start_date, current_month_end_date):
-    ras = gdal.Open(str(output_geotiff_file), gdal.GA_Update)
-    ras.SetMetadataItem('Conventions', str(fh.Conventions))
-    ras.SetMetadataItem('Metadata_Conventions', str(fh.Metadata_Conventions))
-    ras.SetMetadataItem('standard_name_vocabulary', str(fh.standard_name_vocabulary))
-    ras.SetMetadataItem('title', str(fh.title))
-    ras.SetMetadataItem('summary', str(fh.summary))
-    ras.SetMetadataItem('keywords', str(fh.keywords))
-    ras.SetMetadataItem('keywords_vocabulary', str(fh.keywords_vocabulary))
-    ras.SetMetadataItem('platform', str(fh.platform))
-    ras.SetMetadataItem('institution', str(fh.institution))
-    ras.SetMetadataItem('creator_name', str(fh.creator_name))
-    ras.SetMetadataItem('creator_email', str(fh.creator_email))
-    ras.SetMetadataItem('creator_url', str(fh.creator_url))
-    ras.SetMetadataItem('creator_type', str(fh.creator_type))
-    ras.SetMetadataItem('creator_institution', str(fh.creator_institution))
-    ras.SetMetadataItem('publisher_name', str(fh.publisher_name))
-    ras.SetMetadataItem('publisher_email', str(fh.publisher_email))
-    ras.SetMetadataItem('publisher_url', str(fh.publisher_url))
-    ras.SetMetadataItem('publisher_type', str(fh.publisher_type))
-    ras.SetMetadataItem('publisher_institution', str(fh.publisher_institution))
-    ras.SetMetadataItem('project', str(fh.project))
-    ras.SetMetadataItem('program', str(fh.program))
-    ras.SetMetadataItem('id', str(fh.id))
-    ras.SetMetadataItem('naming_authority', str(fh.naming_authority))
-    ras.SetMetadataItem('source', str(fh.source))
-    ras.SetMetadataItem('processing_level', str(fh.processing_level))
-    ras.SetMetadataItem('acknowledgement', str(fh.acknowledgement))
-    ras.SetMetadataItem('license', str(fh.license))
-    ras.SetMetadataItem('product_version', str(fh.product_version))
-    ras.SetMetadataItem('time_epoch', str(fh.time_epoch))
-    ras.SetMetadataItem('time_coverage_start', str(current_month_start_date))
-    ras.SetMetadataItem('time_coverage_end', str(current_month_end_date))
-    ras.SetMetadataItem('geospatial_lat_min', str(fh.geospatial_lat_min))
-    ras.SetMetadataItem('geospatial_lat_max', str(fh.geospatial_lat_max))
-    ras.SetMetadataItem('geospatial_lat_units', str(fh.geospatial_lat_units))
-    ras.SetMetadataItem('geospatial_lat_resolution', str(fh.geospatial_lat_resolution))
-    ras.SetMetadataItem('geospatial_lon_min', str(-179.5))
-    ras.SetMetadataItem('geospatial_lon_max', str(179.5))
-    ras.SetMetadataItem('geospatial_lon_units', str(fh.geospatial_lon_units))
-    ras.SetMetadataItem('geospatial_lon_resolution', str(fh.geospatial_lon_resolution))
-    ras.SetMetadataItem('time_mean_removed', str(fh.time_mean_removed))
-    ras.SetMetadataItem('postprocess_1', str(fh.postprocess_1))
-    ras.SetMetadataItem('postprocess_2', str(fh.postprocess_2))
-    ras.SetMetadataItem('postprocess_3', str(fh.postprocess_3))
-    ras.SetMetadataItem('GIA_removed', str(fh.GIA_removed))
-    ras.SetMetadataItem('C_20_substitution', str(fh.C_20_substitution))
-    ras.SetMetadataItem('C_30_substitution', str(fh.C_30_substitution))
-    ras.SetMetadataItem('user_note_1', str(fh.user_note_1))
-    ras.SetMetadataItem('user_note_2', str(fh.user_note_2))
+def gtiff_set_metadata(output_geotiff_file, ncdf, current_month_start_date, current_month_end_date, lon):
+    attr_list = ncdf.ncattrs()
+    attr_to_exclude = [
+        'time_coverage_start',
+        'time_coverage_end', 
+        'geospatial_lon_min', 
+        'geospatial_lon_max', 
+        'date_created', 
+        'months_missing'
+    ]
+
+    gtiff_name = str(output_geotiff_file)
+    ras = gdal.Open(gtiff_name, gdal.GA_Update)
+
+    for attr_name in attr_list:
+        if attr_name not in attr_to_exclude: 
+            ras.SetMetadataItem(attr_name, str(ncdf.getncattr(attr_name)))
+    
+    ras.SetMetadataItem('time_coverage_start', current_month_start_date)
+    ras.SetMetadataItem('time_coverage_end', current_month_end_date)
+    ras.SetMetadataItem('geospatial_lon_min', str(np.min(lon)))
+    ras.SetMetadataItem('geospatial_lon_max', str(np.max(lon)))
+    
     now = datetime.datetime.now()
-    ras.SetMetadataItem('date_created', str(now.strftime("%Y-%m-%dT%H:%M:%S")))
+    ras.SetMetadataItem('date_created', now.strftime("%Y-%m-%dT%H:%M:%S"))
+    
     ras = None
 
-# this segment opens the netcdf file and reads its variables.
+def main():
+    # this segment opens the netcdf file and reads its variables.
+    ncdf = Dataset(input_mascon_file, mode='r')
 
-fh = Dataset(input_mascon_file, mode='r')
+    input_time = ncdf.variables['time'][:]
+    lat = ncdf.variables['lat'][:]
+    lon = ncdf.variables['lon'][:]
+    lon_bounds= ncdf.variables['lon_bounds'][:]
+    lat_bounds= ncdf.variables['lat_bounds'][:]
+    input_lwe_thickness = ncdf.variables['lwe_thickness'][:] #time lat lon
+    input_uncertainty = ncdf.variables['uncertainty'][:]
 
-input_time = fh.variables['time'][:]
-input_lwe_thickness = fh.variables['lwe_thickness'][:] #time lat lon
-input_uncertainty = fh.variables['uncertainty'][:]
+    time_bounds = ncdf.variables['time_bounds'][:]
+    dtime = num2date(time_bounds[:], ncdf.variables['time_bounds'].units)
 
-time_vals = fh.variables['time'][:]
-time_bounds = fh.variables['time_bounds'][:]
-time_epoch = date.toordinal(date(2002, 1, 1)) + 366
+    # initialize the arrays of same size as input dataset
+    grid_lwe = input_lwe_thickness * 0
+    grid_uncertainty = input_uncertainty * 0
+    timesize = len(input_time)
 
-# initialize the arrays of same size as input dataset
-grid_lwe = input_lwe_thickness * 0
-grid_uncertainty = input_uncertainty * 0
-timesize = len(input_time)
+    data_res  = abs(lon[2 ] - lon[1]) #resolution of dataset
+    indexes_to_shift=int(360 / (2 * data_res)) # no of global longitude divided by degree resolution; /2 gives mid point around which shift is done.
 
-# this loop arrange dataset in 90 to -90 latitude and -180 to 180 longitude orientation and writes monthly geotiffs
-for time_index in range(0, timesize):
-    temp_lwe = input_lwe_thickness[time_index, :, :]
-    temp_uncertainty = input_uncertainty[time_index, :, :]
+    #detremine if grid is south to north then flip the array else not 
+    flip_lat = lat[0] < lat[-1]
 
-    # mascon netcdf contains grid : 0 to 359 longitudes and -90 to 90 degree latitudes.
-    # following lines sets the grid as 180 to -180 longitude and 90 to -90 latitude to be written into geotif
-    temp_1a = np.flipud(temp_lwe)
-    temp_1a_uncert = np.flipud(temp_uncertainty)
+    #detremine if longitudes starts at 0 and goes to n then circshift or roll the grid and longitude array  
+    shift_lon = np.max(lon) > 180
+
+    #this loop arrange dataset in 90 to -90 latitude and -180 to 180 longitude orientation and writes monthly geotiffs; 
+    #works on array in the order: time*lat*lon ; 
+    if input_lwe_thickness.shape == (len(input_time), len(lat), len(lon)):
+        for time_index in range(0,timesize):
+            temp_lwe = input_lwe_thickness[time_index,:,:]
+            temp_uncertainty = input_uncertainty[time_index,:,:]
+            
+            # mascon netcdf contains grid : 0 to 359 longitudes and -90 to 90 degree latitudes. following lines sets the grid as 180 to -180 longitude and 90 to -90 latitude to be written into geotif 
+            if flip_lat:
+                temp_1a = np.flipud(temp_lwe)
+                temp_1a_uncert = np.flipud(temp_uncertainty)
+            else:
+                temp_1a = temp_lwe
+                temp_1a_uncert = temp_uncertainty
     
-    grid_lwe[time_index, :, 360:720] = temp_1a[:, 0:360]
-    grid_lwe[time_index, :, 0:360] = temp_1a[:, 360:720]
-    
-    grid_uncertainty[time_index, :, 360:720] = temp_1a_uncert[:, 0:360]
-    grid_uncertainty[time_index, :, 0:360] = temp_1a_uncert[:, 360:720]
+            if shift_lon:
+                temp_1a = np.roll(temp_1a, indexes_to_shift, axis =1)
+                temp_1a_uncert = np.roll(temp_1a_uncert, indexes_to_shift,axis =1)
+                # arrange longitudes into -180 to 180 orientation 
+                lon[np.where(lon>180)] = lon[np.where(lon > 180)] - indexes_to_shift
+                lon = np.roll(lon, indexes_to_shift, axis=0)
+                lon_bounds[np.where(lon_bounds >= 180)] = lon_bounds[np.where(lon_bounds >= 180)] - indexes_to_shift
+                lon_bounds = np.roll(lon_bounds, indexes_to_shift, axis=0)
+            
+            grid_lwe[time_index,:,:] = temp_1a
+            grid_uncertainty[time_index,:,:] = temp_1a_uncert
+            lat_bound_max = np.max(lat_bounds)
+            lon_bound_min = np.min(lon_bounds)
+                    
+            #determine the start and end period for each monthly timestep and use it in geotiff output filenames 
+            start_year = dtime[time_index,0].year
+            end_year = dtime[time_index,1].year
+            
+            start_day_of_year = dtime[time_index,0].timetuple().tm_yday
+            end_day_of_year = dtime[time_index,1].timetuple().tm_yday
+            
+            start_timestring = str(start_year)  + ("%03d" %(start_day_of_year,))
+            end_timestring = str(end_year)  + ("%03d" %(end_day_of_year,)) 
+            
+            current_month_start_date = dtime[time_index,0].strftime("%Y-%m-%dT%H:%M:%S")
+            current_month_end_date = dtime[time_index,1].strftime("%Y-%m-%dT%H:%M:%S")
+            
+            #generate lwe_thickness tiff file 
+            output_geotiff_file = 'mascon_lwe_thickness_' + start_timestring + '_' + end_timestring +  '.tif'
+            output_geotiff_file = output_geotiff_path / output_geotiff_file
 
-    # determine the start and end period for each monthly timestep and use it in geotiff output filenames
-    start_time = time_bounds[time_index, 0] + time_epoch
-    end_time = time_bounds[time_index, 1] + time_epoch
-    
-    start_timestamp = pd.to_datetime(start_time - 719529, unit='D')
-    end_timestamp = pd.to_datetime(end_time - 719529, unit='D')
-    
-    start_year = start_timestamp.year
-    end_year = end_timestamp.year
-    
-    start_day_of_year = start_timestamp.dayofyear
-    end_day_of_year = end_timestamp.dayofyear
-    
-    start_timestring = str(start_year)  + ("%03d" %(start_day_of_year,))
-    end_timestring = str(end_year)  + ("%03d" %(end_day_of_year,))
-    
-    current_month_start_date = str(start_timestamp.strftime("%Y-%m-%dT%H:%M:%S"))
-    current_month_end_date = str(end_timestamp.strftime("%Y-%m-%dT%H:%M:%S"))
-    
-    # generate lwe_thickness tiff file
-    
-    # resolution of dataset is 0.5 degree
-    sr = 0.5 
+            # create geotiff
+            gtiff_write(grid_lwe[time_index, :, :], data_res, output_geotiff_file, lat_bound_max, lon_bound_min)
+            gtiff_set_metadata(output_geotiff_file, ncdf, current_month_start_date, current_month_end_date,lon)
 
-    output_geotiff_file = 'mascon_lwe_thickness_' + start_timestring + '_' + end_timestring +  '.tif'
-    output_geotiff_file = output_geotiff_path / output_geotiff_file
+            output_geotiff_uncert_file = 'mascon_uncertainty_' + start_timestring + '_' + end_timestring +  '.tif'
+            output_geotiff_uncert_file = output_geotiff_path / output_geotiff_uncert_file
 
-    # create geotiff
-    GTiff_Write(grid_lwe[time_index, :, :], sr, output_geotiff_file)
-    GTiff_SetMetadata(output_geotiff_file, fh, current_month_start_date, current_month_end_date)
+            # create geotiff
+            gtiff_write(grid_uncertainty[time_index,:,:], data_res, output_geotiff_uncert_file, lat_bound_max, lon_bound_min)
+            gtiff_set_metadata(output_geotiff_uncert_file, ncdf, current_month_start_date, current_month_end_date,lon)
+    
+    else:
+        print('Array Dimensions Not in Desired Order; Time * Lat * Lon expected') 
 
-    output_geotiff_uncert_file = 'mascon_uncertainty_' + start_timestring + '_' + end_timestring +  '.tif'
-    output_geotiff_uncert_file = output_geotiff_path / output_geotiff_uncert_file
+    ncdf.close()    
 
-    # create geotiff
-    GTiff_Write(grid_uncertainty[time_index,:,:], sr, output_geotiff_uncert_file)
-    GTiff_SetMetadata(output_geotiff_uncert_file, fh, current_month_start_date, current_month_end_date)
-
-fh.close()
+if __name__ == '__main__':
+    main()
